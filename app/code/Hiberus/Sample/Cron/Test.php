db18eae428f1c450b88dd2119923338ecb79b938
daniDLL <ddelgado@hiberus.com>
2020-11-08 12:39:09 +0100

    cron jobs

---
+++ app/code/Hiberus/Sample/Cron/Test.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author: daniDLL
+ * Date: 8/11/20
+ * Time: 12:28
+ */
+
+namespace Hiberus\Sample\Cron;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class Test
+ * @package Hiberus\Sample\Cron
+ */
+class Test
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * Test constructor.
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Write to system.log
+     *
+     * @return void
+     */
+    public function execute() {
+        $this->logger->info('Cron Works');
+    }
+}
